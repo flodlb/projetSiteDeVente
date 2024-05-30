82ac6a314893799f92813782f83b77db8943d806
flodlb <florianbdlb2@gmail.com>
2024-05-30 18:59:44 +0200

    c'est une belle améliaration connection

--- application/urls.py
+++ application/urls.py
@@ -5,6 +5,7 @@ app_name = "application"
 
 urlpatterns = [
     path('vetements/', views.viewVetements, name='viewVetements'),
+    path('panier/', views.viewPanier, name='viewPanier'),
     path("", views.list_view, name="index"),
     path("", views.home, name="home"),
 ]
